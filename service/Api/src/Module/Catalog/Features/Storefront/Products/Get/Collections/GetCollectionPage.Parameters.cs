dfc2968f6e0aca8ff548c0cc4016655b81cb7f2c
阮青發 <ruanqingfa@gmail.com>
2026-07-04 14:21:23 +0700

    feat(catalog): remove duplicated Search, Filter, Collections, and Digitals endpoints

--- service/Api/src/Module/Catalog/Features/Storefront/Products/Get/Collections/GetCollectionPage.Parameters.cs
+++
@@ -1,6 +0,0 @@
-namespace Module.Catalog.Features.Storefront.Products.Get.Collections;
-
-public static partial class GetCollectionPage
-{
-    public record Parameters : QueryingParameters;
-}
--- service/Api/src/Module/Catalog/Features/Storefront/Products/Get/Filter/FilterProducts.Response.cs
+++
@@ -1,8 +0,0 @@
-using Module.Catalog.Features.Storefront.Products.Shared.Models;
-
-namespace Module.Catalog.Features.Storefront.Products.Get.Filter;
-
-public static partial class FilterProducts
-{
-public record Response : StoreProductListItemResponse;
-}
