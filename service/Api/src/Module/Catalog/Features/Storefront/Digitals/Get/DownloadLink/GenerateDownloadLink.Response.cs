9a647a54b3a951884ed400317918952eadc17d5d
阮青發 <ruanqingfa@gmail.com>
2026-07-04 14:19:42 +0700

    fix(catalog): delete old Digitals directory as part of Images rename

--- service/Api/src/Module/Catalog/Features/Storefront/Digitals/Get/DownloadLink/GenerateDownloadLink.Response.cs
+++
@@ -1,8 +0,0 @@
-using Module.Catalog.Features.Storefront.Digitals.Shared.Models;
-
-namespace Module.Catalog.Features.Storefront.Digitals.Get.DownloadLink;
-
-public static partial class GenerateDownloadLink
-{
-    public class Response : StoreDigitalDownloadResponse { }
-}
