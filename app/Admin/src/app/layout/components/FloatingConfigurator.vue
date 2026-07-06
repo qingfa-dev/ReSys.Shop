5512396ab36c96b18aaf9c8849f9a6a1e9f6cf05
阮青發 <ruanqingfa@gmail.com>
2026-07-07 01:05:06 +0700

    feat(admin): add FloatingConfigurator with dark mode + palette quick access

---
+++ app/Admin/src/app/layout/components/FloatingConfigurator.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import AppConfigurator from '@/app/layout/configurator.layout.vue'
+import { useLayout } from '@/app/layout/composables/layout.composable'
+
+const { toggleDarkMode, isDarkTheme } = useLayout()
+</script>
+
+<template>
+  <div class="fixed flex gap-4 top-8 right-8 z-50">
+    <Button
+      type="button"
+      @click="toggleDarkMode"
+      rounded
+      :icon="isDarkTheme ? 'pi pi-moon' : 'pi pi-sun'"
+      severity="secondary"
+    />
+    <div class="relative">
+      <Button
+        icon="pi pi-palette"
+        v-styleclass="{
+          selector: '@next',
+          enterFromClass: 'hidden',
+          enterActiveClass: 'animate-scalein',
+          leaveToClass: 'hidden',
+          leaveActiveClass: 'animate-fadeout',
+          hideOnOutsideClick: true,
+        }"
+        type="button"
+        rounded
+      />
+      <AppConfigurator />
+    </div>
+  </div>
+</template>
