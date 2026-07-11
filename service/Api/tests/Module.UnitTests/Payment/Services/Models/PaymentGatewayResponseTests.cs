9d97417aef934dfc4256083b8d44f8022541e933
阮青發 <ruanqingfa@gmail.com>
2026-07-11 15:51:15 +0700

    feat(payment): add ClientSecret to PaymentGatewayResponse

---
+++ service/Api/tests/Module.UnitTests/Payment/Services/Models/PaymentGatewayResponseTests.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+using Module.Payment.Services.Models;
+
+namespace Module.UnitTests.Payment.Services.Models;
+
+public class PaymentGatewayResponseTests
+{
+    [Fact]
+    public void Constructor_Should_Set_ClientSecret_When_Provided()
+    {
+        var response = new PaymentGatewayResponse(
+            provider: "bogus",
+            authorization: "auth_123",
+            clientSecret: "pi_fake_secret_123");
+
+        response.ClientSecret.Should().Be("pi_fake_secret_123");
+    }
+}
