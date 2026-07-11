d72c5383e8bef5e7a44759b81cb3e17db112eb57
阮青發 <ruanqingfa@gmail.com>
2026-07-11 18:26:07 +0700

    test(shipping): add admin shipping method and shipping rate mapping unit tests
    Tests MapToDetail, MapToListItem, MapToDomain (create + update), and
    MapUpdateToDomain (partial patch) for both ShippingMethod and ShippingRate.
    Verifies 14+ properties including timestamps, DeliveryRange, cost, weight,
    and free-shipping-threshold fields.

---
+++ service/Api/tests/Module.UnitTests/Shipping/Features/Admin/ShippingMethods/Shared/Mappings/ShippingMethod.Mapping.Tests.cs
@@ -0,0 +1,140 @@
+using Module.Shipping.Domain.ShippingMethods;
+using ShippingDomain = Module.Shipping.Domain.ShippingMethods.ShippingMethod;
+using Module.Shipping.Features.Admin.ShippingMethods.Shared.Mappings;
+using Module.Shipping.Features.Admin.ShippingMethods.Shared.Models;
+
+namespace Module.UnitTests.Shipping.Features.Admin.ShippingMethods.Shared.Mappings;
+
+[Trait("Category", "Unit")]
+[Trait("Module", "Shipping")]
+[Trait("Feature", "ShippingMethodMapping")]
+public class ShippingMethodMappingTests
+{
+    [Fact(DisplayName = "MapToDetail: Should map ShippingMethod to detail response")]
+    public void MapToDetail_ShouldMapEntityToDetail()
+    {
+        var method = CreateShippingMethod();
+
+        var response = method.MapToDetail<ShippingMethodDetailResponse>();
+
+        response.Should().NotBeNull();
+        response.Id.Should().Be(method.Id);
+        response.Name.Should().Be(method.Name);
+        response.Code.Should().Be(method.Code);
+        response.TrackingUrl.Should().Be(method.TrackingUrl);
+        response.AdminName.Should().Be(method.AdminName);
+        response.Position.Should().Be(method.Position);
+        response.AvailableToUsers.Should().Be(method.AvailableToUsers);
+        response.CalculatorType.Should().Be(method.CalculatorType);
+        response.TaxCategoryId.Should().Be(method.TaxCategoryId);
+        response.Presentation.Should().Be(method.Presentation);
+        response.CreatedAtUtc.Should().Be(method.CreatedAtUtc);
+        response.ModifiedAtUtc.Should().Be(method.ModifiedAtUtc);
+        response.CreatedBy.Should().Be(method.CreatedBy);
+        response.ModifiedBy.Should().Be(method.ModifiedBy);
+        response.IsDeleted.Should().Be(method.IsDeleted);
+        response.DeletedAtUtc.Should().Be(method.DeletedAtUtc);
+    }
+
+    [Fact(DisplayName = "MapToListItem: Should map ShippingMethod to list item response")]
+    public void MapToListItem_ShouldMapEntityToList()
+    {
+        var method = CreateShippingMethod();
+
+        var response = method.MapToListItem<ShippingMethodListItemResponse>();
+
+        response.Should().NotBeNull();
+        response.Id.Should().Be(method.Id);
+        response.Name.Should().Be(method.Name);
+        response.Code.Should().Be(method.Code);
+        response.TrackingUrl.Should().Be(method.TrackingUrl);
+        response.AdminName.Should().Be(method.AdminName);
+        response.Position.Should().Be(method.Position);
+        response.AvailableToUsers.Should().Be(method.AvailableToUsers);
+        response.CalculatorType.Should().Be(method.CalculatorType);
+        response.TaxCategoryId.Should().Be(method.TaxCategoryId);
+        response.Presentation.Should().Be(method.Presentation);
+        response.CreatedAtUtc.Should().Be(method.CreatedAtUtc);
+        response.ModifiedAtUtc.Should().Be(method.ModifiedAtUtc);
+    }
+
+    [Fact(DisplayName = "MapToDomain: Should map request to new ShippingMethod entity")]
+    public void MapToDomain_Create_ShouldMapRequestToEntity()
+    {
+        var request = new ShippingMethodRequest
+        {
+            Name = "Express",
+            CalculatorType = "flat_rate",
+            Code = "EXP",
+            TaxCategoryId = Guid.NewGuid(),
+            TrackingUrl = "https://track.example.com",
+            AdminName = "Express Admin",
+            Position = 1,
+            AvailableToUsers = true,
+            Presentation = "Express Delivery"
+        };
+
+        var result = request.MapToDomain();
+        var entity = result.Value;
+
+        result.IsSuccess.Should().BeTrue();
+        entity.Should().NotBeNull();
+        entity.Name.Should().Be(request.Name);
+        entity.CalculatorType.Should().Be(request.CalculatorType);
+        entity.Code.Should().Be(request.Code);
+        entity.TaxCategoryId.Should().Be(request.TaxCategoryId);
+        entity.TrackingUrl.Should().Be(request.TrackingUrl);
+        entity.AdminName.Should().Be(request.AdminName);
+        entity.Position.Should().Be(request.Position);
+        entity.AvailableToUsers.Should().Be(request.AvailableToUsers);
+        entity.Presentation.Should().Be(request.Presentation);
+    }
+
+    [Fact(DisplayName = "MapToDomain (Update): Should update existing ShippingMethod")]
+    public void MapToDomain_Update_ShouldUpdateEntity()
+    {
+        var method = CreateShippingMethod();
+        var request = new ShippingMethodRequest
+        {
+            Name = "Updated",
+            CalculatorType = "weight",
+            Code = "UPD",
+            TaxCategoryId = Guid.NewGuid()
+        };
+
+        var result = request.MapToDomain(method);
+
+        result.IsSuccess.Should().BeTrue();
+        method.Name.Should().Be("Updated");
+        method.CalculatorType.Should().Be("weight");
+    }
+
+    [Fact(DisplayName = "MapUpdateToDomain: Should apply partial update")]
+    public void MapUpdateToDomain_ShouldApplyPatch()
+    {
+        var method = CreateShippingMethod();
+        var patchRequest = new ShippingMethodUpdateRequest
+        {
+            Name = "Patched Name"
+        };
+
+        var result = patchRequest.MapUpdateToDomain(method);
+
+        result.IsSuccess.Should().BeTrue();
+        method.Name.Should().Be("Patched Name");
+        method.Code.Should().NotBeNull();
+    }
+
+    private static ShippingDomain CreateShippingMethod()
+    {
+        var result = ShippingMethodExtensions.Create("Express", "flat_rate", "EXP", Guid.NewGuid());
+        result.IsSuccess.Should().BeTrue();
+        var method = result.Value;
+        method.TrackingUrl = "https://track.example.com";
+        method.AdminName = "Express Admin";
+        method.Position = 1;
+        method.AvailableToUsers = true;
+        method.Presentation = "Express Delivery";
+        return method;
+    }
+}
---
+++ service/Api/tests/Module.UnitTests/Shipping/Features/Admin/ShippingRates/Shared/Mappings/ShippingRate.Mapping.Tests.cs
@@ -0,0 +1,136 @@
+using Module.Shipping.Domain.ShippingRates;
+using RateDomain = Module.Shipping.Domain.ShippingRates.ShippingRate;
+using Module.Shipping.Features.Admin.ShippingRates.Shared.Mappings;
+using Module.Shipping.Features.Admin.ShippingRates.Shared.Models;
+
+namespace Module.UnitTests.Shipping.Features.Admin.ShippingRates.Shared.Mappings;
+
+[Trait("Category", "Unit")]
+[Trait("Module", "Shipping")]
+[Trait("Feature", "ShippingRateMapping")]
+public class ShippingRateMappingTests
+{
+    [Fact(DisplayName = "MapToDetail: Should map ShippingRate to detail response")]
+    public void MapToDetail_ShouldMapEntityToDetail()
+    {
+        var rate = CreateShippingRate();
+
+        var response = rate.MapToDetail<ShippingRateDetailResponse>();
+
+        response.Should().NotBeNull();
+        response.Id.Should().Be(rate.Id);
+        response.Name.Should().Be(rate.Name);
+        response.Cost.Should().Be(rate.Cost);
+        response.FinalPrice.Should().Be(rate.FinalPrice);
+        response.Selected.Should().Be(rate.Selected);
+        response.DeliveryRange.Should().Be(rate.DeliveryRange);
+        response.MinWeight.Should().Be(rate.MinWeight);
+        response.MaxWeight.Should().Be(rate.MaxWeight);
+        response.FreeShippingThreshold.Should().Be(rate.FreeShippingThreshold);
+        response.ShippingMethodId.Should().Be(rate.ShippingMethodId);
+        response.CreatedAtUtc.Should().Be(rate.CreatedAtUtc);
+        response.ModifiedAtUtc.Should().Be(rate.ModifiedAtUtc);
+        response.CreatedBy.Should().Be(rate.CreatedBy);
+        response.ModifiedBy.Should().Be(rate.ModifiedBy);
+    }
+
+    [Fact(DisplayName = "MapToListItem: Should map ShippingRate to list item response")]
+    public void MapToListItem_ShouldMapEntityToList()
+    {
+        var rate = CreateShippingRate();
+
+        var response = rate.MapToListItem<ShippingRateListItemResponse>();
+
+        response.Should().NotBeNull();
+        response.Id.Should().Be(rate.Id);
+        response.Name.Should().Be(rate.Name);
+        response.Cost.Should().Be(rate.Cost);
+        response.FinalPrice.Should().Be(rate.FinalPrice);
+        response.Selected.Should().Be(rate.Selected);
+        response.DeliveryRange.Should().Be(rate.DeliveryRange);
+        response.MinWeight.Should().Be(rate.MinWeight);
+        response.MaxWeight.Should().Be(rate.MaxWeight);
+        response.FreeShippingThreshold.Should().Be(rate.FreeShippingThreshold);
+        response.ShippingMethodId.Should().Be(rate.ShippingMethodId);
+        response.CreatedAtUtc.Should().Be(rate.CreatedAtUtc);
+        response.ModifiedAtUtc.Should().Be(rate.ModifiedAtUtc);
+    }
+
+    [Fact(DisplayName = "MapToDomain: Should map request to new ShippingRate entity")]
+    public void MapToDomain_Create_ShouldMapRequestToEntity()
+    {
+        var shippingMethodId = Guid.NewGuid();
+        var request = new ShippingRateRequest
+        {
+            Name = "Standard",
+            Cost = 10.00m,
+            ShippingMethodId = shippingMethodId,
+            DeliveryRange = "3-5 days",
+            MinWeight = 0.1m,
+            MaxWeight = 10.0m,
+            FreeShippingThreshold = 100.0m
+        };
+
+        var result = request.MapToDomain();
+        var entity = result.Value;
+
+        result.IsSuccess.Should().BeTrue();
+        entity.Should().NotBeNull();
+        entity.Name.Should().Be(request.Name);
+        entity.Cost.Should().Be(request.Cost);
+        entity.ShippingMethodId.Should().Be(request.ShippingMethodId);
+        entity.DeliveryRange.Should().Be(request.DeliveryRange);
+        entity.MinWeight.Should().Be(request.MinWeight);
+        entity.MaxWeight.Should().Be(request.MaxWeight);
+        entity.FreeShippingThreshold.Should().Be(request.FreeShippingThreshold);
+    }
+
+    [Fact(DisplayName = "MapToDomain (Update): Should update existing ShippingRate")]
+    public void MapToDomain_Update_ShouldUpdateEntity()
+    {
+        var rate = CreateShippingRate();
+        var request = new ShippingRateRequest
+        {
+            Name = "Updated Rate",
+            Cost = 15.00m,
+            ShippingMethodId = rate.ShippingMethodId,
+            DeliveryRange = "1-2 days",
+            MinWeight = 0.5m,
+            MaxWeight = 20.0m,
+            FreeShippingThreshold = 200.0m
+        };
+
+        var result = request.MapToDomain(rate);
+
+        result.IsSuccess.Should().BeTrue();
+        rate.Name.Should().Be("Updated Rate");
+        rate.Cost.Should().Be(15.00m);
+        rate.DeliveryRange.Should().Be("1-2 days");
+    }
+
+    [Fact(DisplayName = "MapUpdateToDomain: Should apply partial update")]
+    public void MapUpdateToDomain_ShouldApplyPatch()
+    {
+        var rate = CreateShippingRate();
+        var patchRequest = new ShippingRateUpdateRequest
+        {
+            Name = "Patched Rate",
+            Cost = 12.00m
+        };
+
+        var result = patchRequest.MapUpdateToDomain(rate);
+
+        result.IsSuccess.Should().BeTrue();
+        rate.Name.Should().Be("Patched Rate");
+        rate.Cost.Should().Be(12.00m);
+        rate.ShippingMethodId.Should().NotBeEmpty();
+    }
+
+    private static RateDomain CreateShippingRate()
+    {
+        var result = ShippingRateExtensions.Create(
+            "Standard", 10.00m, Guid.NewGuid(), "3-5 days", 0.1m, 10.0m, 100.0m);
+        result.IsSuccess.Should().BeTrue();
+        return result.Value;
+    }
+}
