d06304ef461ac47dc6c6cab365844626c86bfc73
阮青發 <ruanqingfa@gmail.com>
2026-07-20 19:21:30 +0700

    feat: add PageContainer component with tests

---
+++ app/Admin/src/shared/components/layout/PageContainer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  maxWidth?: string
+  card?: boolean
+}>(), {
+  maxWidth: '1504px',
+  card: true,
+})
+
+defineSlots<{
+  default(): any
+}>()
+</script>
+
+<template>
+  <div
+    class="page-container p-6 mx-auto"
+    :style="{ maxWidth }"
+  >
+    <div v-if="card" class="card">
+      <slot />
+    </div>
+    <slot v-else />
+  </div>
+</template>
