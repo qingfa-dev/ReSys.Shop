73d3ff2a2953a6c71049118eddaa26550d234026
阮青發 <ruanqingfa@gmail.com>
2026-07-20 19:29:09 +0700

    feat: add DescriptionList component with tests

--- app/Admin/src/shared/components/data-display/index.ts
+++ app/Admin/src/shared/components/data-display/index.ts
@@ -1,2 +1,3 @@
 export { default as DetailField } from './DetailField.vue'
 export { default as DetailGroup } from './DetailGroup.vue'
+export { default as DescriptionList } from './DescriptionList.vue'
