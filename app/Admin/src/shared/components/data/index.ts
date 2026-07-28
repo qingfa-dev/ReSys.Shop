e675f44169feb17d07a21506b74610b692cd8635
阮青發 <ruanqingfa@gmail.com>
2026-07-28 19:11:03 +0700

    refactor(admin): remove CrudToolbar and FilterableDataTable wrappers

--- app/Admin/src/shared/components/data/index.ts
+++ app/Admin/src/shared/components/data/index.ts
@@ -1,4 +1,2 @@
-export { default as FilterableDataTable } from './FilterableDataTable.vue'
-export { default as CrudToolbar } from './CrudToolbar.vue'
 export { default as StatusTag } from './StatusTag.vue'
 export { default as RatingBadge } from './RatingBadge.vue'
