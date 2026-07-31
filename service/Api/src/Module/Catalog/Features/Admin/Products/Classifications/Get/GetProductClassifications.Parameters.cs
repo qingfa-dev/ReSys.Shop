163e677c17387ea861fbb34eaa47792a9b5afd52
阮青發 <ruanqingfa@gmail.com>
2026-07-31 16:37:11 +0700

    refactor(catalog): return PagedResult from GetProductClassifications

---
+++ service/Api/src/Module/Catalog/Features/Admin/Products/Classifications/Get/GetProductClassifications.Parameters.cs
@@ -0,0 +1,6 @@
+namespace Module.Catalog.Features.Admin.Products.Classifications.Get;
+
+public static partial class GetProductClassifications
+{
+    public sealed record Parameters : QueryingParameters;
+}
