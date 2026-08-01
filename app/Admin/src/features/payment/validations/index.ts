c105b2fa4f0bf0df7fa7e27ecfc8ffa07cd2f9a9
阮青發 <ruanqingfa@gmail.com>
2026-08-01 20:45:00 +0700

    feat(payment): add payment method form validations

--- app/Admin/src/features/payment/validations/index.ts
+++ app/Admin/src/features/payment/validations/index.ts
@@ -1,1 +1,14 @@
-// Barrel exports for payment/validations
+export {
+  paymentMethodName,
+  paymentMethodCode,
+  paymentMethodProviderKey,
+  paymentMethodDescription,
+  paymentMethodDisplayOn,
+  paymentMethodPosition,
+  paymentMethodActive,
+  paymentMethodWebhookEnabled,
+  paymentMethodAutoCapture,
+  paymentMethodSchema,
+  paymentMethodUpdateSchema,
+} from './paymentMethod'
+export type { PaymentMethodForm, PaymentMethodUpdateForm } from './paymentMethod'
