09b2022bde8c8f508f6e8b66894ae433ff6e6480
阮青發 <ruanqingfa@gmail.com>
2026-08-07 06:39:14 +0700

    feat(shared): replace ScrollToTop with PrimeVue ScrollTop

--- app/Store/src/shared/components/ScrollToTop.vue
+++ app/Store/src/shared/components/ScrollToTop.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import { watch } from 'vue'
-import { useRoute } from 'vue-router'
-const route = useRoute()
-watch(() => route.fullPath, () => window.scrollTo(0, 0))
-</script>
-<template><div /></template>
+<template>
+  <ScrollTop :threshold="500" icon="pi pi-arrow-up" />
+</template>
